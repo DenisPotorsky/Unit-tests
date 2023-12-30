c625fa5fd1e8ca7137f850bfd61541e5ee65b635
DenisPotorsky <denis-s2@yandex.ru>
2023-12-30 15:32:50 +0500

    homework is done

--- sem1/src/main/java/CalculatorTest.java
+++ sem1/src/main/java/CalculatorTest.java
@@ -1,17 +1,38 @@
 package main.java;
 
-import static main.java.Calculator.calculateDiscount;
-import static org.assertj.core.api.Assertions.*;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        assertThat(Calculator.calculate(100, 25)).isEqualTo(75);
+        assertThat(Calculator.calculateDiscount(100, 10)).isEqualTo(90);
+    }
 
-        try {
-            System.out.println(calculateDiscount(10, 0));
-        } catch (ArithmeticException e) {
-            System.out.println(e.getMessage());
-        }
+    @Test
+    public void testResult() {
+        assertEquals(90.0, Calculator.calculateDiscount(100, 10), 0.0);
+    }
+
+    @Test
+    public void testZeroResult() {
+        assertThat(Calculator.calculateDiscount(0, 10)).isEqualTo(0);
+    }
+
+    @Test
+    public void testZeroDiscount() {
+        assertThat(Calculator.calculateDiscount(100, 0)).isEqualTo(100);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDiscountLessThanZero() {
+        Calculator.calculateDiscount(100, -5);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDiscountMoreThan100() {
+        Calculator.calculateDiscount(100, 110);
     }
 }
 
